922b6b1220a028211471c76beb0f0abff32e1585
DeBovuar <debovuar@bk.ru>
2012-11-25 18:17:53 +0200

    game scene added, small menu scene refactoring

---
+++ wysiwyg/Classes/GameScene/GameScene.h
@@ -0,0 +1,24 @@
+#ifndef _GAME_SCENE_H__
+#define _GAME_SCENE_H__
+
+#include "cocos2d.h"
+
+class GameScene : public cocos2d::CCLayer
+{
+public:
+    virtual bool init();  
+
+    static cocos2d::CCScene* scene();
+    
+    // a selector callback
+    //void menuCloseCallback(CCObject* pSender);
+
+    // implement the "static node()" method manually
+    CREATE_FUNC( GameScene );
+};
+
+
+
+
+
+#endif
--- wysiwyg/Classes/MainMenuScene/MainMenuScene.cpp
+++ wysiwyg/Classes/MainMenuScene/MainMenuScene.cpp
@@ -1,4 +1,5 @@
 #include "MainMenuScene.h"
+#include "GameScene.h"
 
 using namespace cocos2d;
 
@@ -22,6 +23,17 @@ CCScene* MainMenuScene::scene()
     // return the scene
     return scene;
 }
+
+void MainMenuScene::menuPlayeCallback( CCObject * pSender )
+{
+	CCScene * pScene = GameScene::scene();
+
+	CCDirector::sharedDirector()->replaceScene( pScene );
+
+	return;
+}
+
+
 
 // on "init" you need to initialize your instance
 bool MainMenuScene::init()
@@ -47,13 +59,24 @@ bool MainMenuScene::init()
             "CloseSelected.png",
             this,
             menu_selector(MainMenuScene::menuCloseCallback));
+
+		CCMenuItemImage * pPlayeItem = CCMenuItemImage::create(
+			"play.png",
+			"play_tapped.png",
+			this,
+			menu_selector(MainMenuScene::menuPlayeCallback) );
+
+
         CC_BREAK_IF(! pCloseItem);
 
         // Place the menu item bottom-right conner.
         pCloseItem->setPosition(ccp(CCDirector::sharedDirector()->getWinSize().width - 20, 20));
 
+		CCSize size = CCDirector::sharedDirector()->getWinSize();
+		pPlayeItem->setPosition( ccp( size.width/2, size.height/2 ) );
+
         // Create a menu with the "close" menu item, it's an auto release object.
-        CCMenu* pMenu = CCMenu::create(pCloseItem, NULL);
+		CCMenu* pMenu = CCMenu::create(pCloseItem, pPlayeItem, NULL);
         pMenu->setPosition(CCPointZero);
         CC_BREAK_IF(! pMenu);
 
@@ -63,27 +86,26 @@ bool MainMenuScene::init()
         // 2. Add a label shows "Hello World".
 
         // Create a label and initialize with string "Hello World".
-        CCLabelTTF* pLabel = CCLabelTTF::create("Hello World", "Arial", 24);
-        CC_BREAK_IF(! pLabel);
+        //CCLabelTTF* pLabel = CCLabelTTF::create("Hello World", "Arial", 24);
+		//CC_BREAK_IF(! pLabel);
 
         // Get window size and place the label upper. 
-        CCSize size = CCDirector::sharedDirector()->getWinSize();
-        pLabel->setPosition(ccp(size.width / 2, size.height - 50));
+        //CCSize size = CCDirector::sharedDirector()->getWinSize();
+        //pLabel->setPosition(ccp(size.width / 2, size.height - 50));
 
         // Add the label to HelloWorld layer as a child layer.
-        this->addChild(pLabel, 1);
+        //this->addChild(pLabel, 1);
 
         // 3. Add add a splash screen, show the cocos2d splash image.
-        CCSprite* pSprite = CCSprite::create("HelloWorld.png");
-        CC_BREAK_IF(! pSprite);
+        //CCSprite* pSprite = CCSprite::create("HelloWorld.png");
+        //CC_BREAK_IF(! pSprite);
 
         // Place the sprite on the center of the screen
-        pSprite->setPosition(ccp(size.width/2, size.height/2));
+        //pSprite->setPosition(ccp(size.width/2, size.height/2));
 
         // Add the sprite to HelloWorld layer as a child layer.
-        this->addChild(pSprite, 0);
-
-        bRet = true;
+        //this->addChild(pSprite, 0);
+	    bRet = true;
     } while (0);
 
     return bRet;
