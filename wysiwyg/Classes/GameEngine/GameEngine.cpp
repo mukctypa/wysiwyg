31df0526b0d5aabfacc76fe208ca2b210350c3cb
mukctypa <denim@tut.by>
2012-11-27 01:36:56 +0300

    gameEngine initinal impl

---
+++ wysiwyg/Classes/GameEngine/GameEngine.cpp
@@ -0,0 +1,38 @@
+#include "GameEngine.h"
+
+
+GameEngine & GameEngine::getInstance()
+{
+	static GameEngine instance;
+	return instance;
+}
+
+void GameEngine::init()
+{
+	//init all stuf
+	this->m_state = ReadyToStart;
+}
+
+
+void GameEngine::start()
+{
+	//start game - create first set of letters, start timer
+	this->m_state = InGame;
+}
+
+
+void GameEngine::logic()
+{
+	switch ( this->m_state )
+	{
+		case ReadyToStart:
+		{
+			//do nothing: game not started
+			break;
+		}
+		case InGame:
+		{
+			//check if game field is not full
+		}
+	}
+}
--- wysiwyg/Classes/GameEngine/GameEngine.h
+++ wysiwyg/Classes/GameEngine/GameEngine.h
@@ -1,6 +1,34 @@
 #ifndef __GAMEENGINE__H__
 #define __GAMEENGINE__H__
 
+typedef enum
+{
+	ReadyToStart,
+	InGame,
+	VaidateUserInput,
+	AddLetters,
+	RemoveLetters,
+	GameEnd,
+} GameState;
 
+
+class GameEngine
+{
+
+protected:
+	void logic();
+	void logicWrapper();
+
+	GameState m_state;
+	GameEngine() 
+	{
+	}
+
+public: 
+	static GameEngine & getInstance();
+	void init(); 
+	void start();
+
+};
 
 #endif
